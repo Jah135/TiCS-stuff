da5181e112fb4f3788ebc9ba43bb193e8ee5ab0a
Elijah <ehopper848@phm.k12.in.us>
2026-09-02 13:12:30 -0400

    NEW PSEUDOGENGLISH

--- forfun/pseudoenglish/thing.py
+++ forfun/pseudoenglish/thing.py
@@ -1,44 +1,12 @@
-from pseudoenglish import analyze_text_patterns, generate_text
+from pseudoenglish import analyze_text, TextGenerator
 
-def intput(prompt: str, default: int | None = None) -> int:
-	while True:
-		try:
-			text = input(prompt)
+data = ""
 
-			if text == "" and default != None:
-				return default
+for file in ["shrek.txt", "seals.txt", "bees.txt"]:
+    with open(file, "r", encoding="utf8") as f:
+        data = f.read()
 
-			return int(text)
-		except:
-			print("Invalid integer.")
+analysis = analyze_text(data, 1)
+generator = TextGenerator(analysis.lingo)
 
-INPUT_DATA = ""
-
-
-while True:
-	try:
-		file_name = input("Target file: ")
-		with open(file_name, "+r") as f:
-			print(f"Reading file '{file_name}'...\n")
-
-			INPUT_DATA += f.read()
-
-			break
-	except Exception:
-		print("Invalid file.")
-
-
-PATTERN_SIZE = intput("Pattern size (default 6): ", 6)
-READBACK_SIZE = intput("Readback size (default 6): ", 6)
-TOKEN_SIZE = intput("Token size (default 3): ", 3)
-
-pattern_associations = analyze_text_patterns(INPUT_DATA, PATTERN_SIZE, TOKEN_SIZE)
-
-print(f"\nInput Data Size: {len(INPUT_DATA)}\nAssociation Count: {len(pattern_associations)}\n\nPattern Size: {PATTERN_SIZE}\nReadback Size: {READBACK_SIZE}\nToken Size: {TOKEN_SIZE}\n")
-
-while True:
-	seed = input("Input seed: ")
-	count = intput("Input count: ")
-
-	output_text = generate_text(pattern_associations, seed, count, READBACK_SIZE)
-	print(f"{'-' * 50}\n{output_text}\n{'-' * 50}")
+print(generator.generate_text("What", 500))
