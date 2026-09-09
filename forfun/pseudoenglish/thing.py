4db537c96a10b70f83c53db192ce6b1b3ffeec6c
Elijah <ehopper848@phm.k12.in.us>
2026-09-09 13:45:00 -0400

    anjdkbjhksjkndrw342568RUGHYK

--- forfun/pseudoenglish/thing.py
+++ forfun/pseudoenglish/thing.py
@@ -1,12 +1,27 @@
 from pseudoenglish import analyze_text, TextGenerator
+from collections import Counter
+from random import choices
+
+class CustomTextGenerator(TextGenerator):
+    def determine_next_word(self) -> str:
+        total_weights: Counter[str] = Counter()
+
+        reach = min(len(self.history) - 1, 5)
+
+        total_weights["a"] = 1
+
+        for word, distance in map(lambda x: (self.history[-x - 1], x), range(reach)):
+            print(word, distance)
+
+        return choices(list(total_weights.keys()), list(total_weights.values()), k=1)[0]
 
 data = ""
 
 for file in ["shrek.txt", "seals.txt", "bees.txt"]:
     with open(file, "r", encoding="utf8") as f:
-        data = f.read()
+        data += f.read()
 
-analysis = analyze_text(data, 1)
-generator = TextGenerator(analysis.lingo)
+analysis = analyze_text(data, 3)
+generator = CustomTextGenerator(analysis.lingo)
 
-print(generator.generate_text("Thinking", 500))
+print(generator.generate_text("Shrek!", 1))
